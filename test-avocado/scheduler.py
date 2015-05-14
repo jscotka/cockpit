05cfc0d8eaa316ca26f97cff645f5c2d34c042b2
Jan Scotka <jscotka@redhat.com>
2015-05-14 14:53:20 +0200

    added example scheduler

---
+++ test-avocado/scheduler.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from nrun import *
+
+spawn_guest("abc","fedora-21")
+spawn_guest("abd","fedora-22")
+
+#echo_log("testing")
+#test_func(guest)
+
+#echo_success("success", always_show = True)
+
